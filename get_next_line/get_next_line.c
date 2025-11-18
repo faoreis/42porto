c72b380d125985f3ceb0a249eb71eed0131102d3
Fabio Ribeiro Reis <faribeir@c1r9s1.42porto.com>
2025-11-18 21:41:02 +0000

    get_next_line

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,17 @@
 /*   By: faribeir <faribeir@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 20:31:11 by faribeir          #+#    #+#             */
-/*   Updated: 2025/11/17 22:30:29 by faribeir         ###   ########.fr       */
+/*   Updated: 2025/11/18 21:04:33 by faribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*get_next_line(int fd)
 {
-	
+	char	*buffer;
+
+	if (!fd || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
 }
