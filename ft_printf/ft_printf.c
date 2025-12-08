24c9cf6b76753df2772436f04a8c0cfb2aaafe83
faoreis <fao.reis123@gmail.com>
2025-12-08 22:51:23 +0000

    Update printf

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -1,14 +1,12 @@
 #include "ft_printf.h" 
 
-int ft_printf(const char *str, ...)
+int ft_print(char const *str, va_list args)
 {
-    va_list args;
     int i;
     int count;
 
     i = 0;
-    count = 0
-    va_start(args, str);
+    count = 0;
     while(str[i])
     {
         if (str[i] != '%')
@@ -17,7 +15,22 @@ int ft_printf(const char *str, ...)
             count++;
         }
         else
-
+        {
+            i++;
+            count += ft_var_type(str, args, i);
+            i++;
+        }
     }
+}
+
+int ft_printf(const char *str, ...)
+{
+    va_list args;
+    int len;
+
+    len = 0;
+    va_start(args, str);
+    len = ft_print(str, args);
     va_end(args);
+    return (len)
 }
