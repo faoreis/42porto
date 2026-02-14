31a286a6f28472c927892099e8a5f502ddaa78ff
Fabio Ribeiro Reis <faribeir@c2r6s1.42porto.com>
2026-02-14 14:31:04 +0000

    cc

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -1,23 +1,51 @@
-char **ft_validate_input(int argc, char **list)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faribeir <faribeir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/12 21:04:11 by faribeir          #+#    #+#             */
+/*   Updated: 2026/02/14 10:13:19 by faribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int    ft_validate_input(int argc, char **list)
 {
-    int i;
+	int     i;
+	int	outresult;
 
-    i = 1;
-    if(argc == 2)
-        i = 0;
-    
+	i = 1;
+	if (argc == 2)
+		i = 0;
+	outresult = 1;
+	while (list[i])
+	{
+		outresult = ft_atoi(list[i]);
+		if (outresult < 0)
+			break;
+		i++;
+	}
+	return (outresult);
 }
-
+   
 int main(int argc,char **argv)
 {
-    char    **list;
+	char    **list;
+	int	error;
 
-    if(argc == 1)
-        return (0);
-    if(argc == 2)
-        list = ft_split(argv[1], ' ');
-    else
-        list = argv;
-    //ft_validate_input(argc, list);
-    return 0;
-}
+	if(argc == 1)   
+		return (0); 
+	if(argc == 2)
+		list = ft_split(argv[1], ' ');
+	else
+	list = argv;  
+	error = ft_validate_input(argc, list);
+	if (error < 0)
+		return (write(1, "error\n", 6));
+	else
+		return (write(1, "ok!", 3));
+	return 0;
+}   
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -1,10 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: faribeir <faribeir@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/14 09:54:02 by faribeir          #+#    #+#             */
+/*   Updated: 2026/02/14 10:11:48 by faribeir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
 # include <unistd.h>
 # include <stddef.h>
 # include <stdlib.h>
+# include "Libft/libft.h"
 
-struct node
-{
-    int x;
-    int n;
-    struct node *next;
-};
+int	ft_validate_input(int argc, char **list);
+
+#endif
