ca9600524969c55aff92679124186fb25a23f142
Fabio Ribeiro Reis <faribeir@c2r3s2.42porto.com>
2026-02-18 21:09:17 +0000

    project

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,21 +6,37 @@
 /*   By: faribeir <faribeir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/12 21:04:11 by faribeir          #+#    #+#             */
-/*   Updated: 2026/02/14 10:13:19 by faribeir         ###   ########.fr       */
+/*   Updated: 2026/02/18 21:05:24 by faribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ft_free(char **list)
+{
+	int	i;
+
+	i = 0;
+	while (list[i])
+	{
+		free(list[i]);
+		i++;
+	}
+	free(list);
+}
+
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (i < n - 1 && s1[i] == s2[i] && s1[i] && s2[i])
+	int	i;
+
+	i = 0;
+	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
 
-int	ft_contains(const char *s1, const char **s2, int i)
+int	ft_contains(const char *s1, char **s2, int i)
 {
 	int	compar;
 
@@ -29,6 +45,7 @@ int	ft_contains(const char *s1, const char **s2, int i)
 	while (s2[i] && compar != 0)
 	{
 		compar = ft_strcmp(s1, s2[i]);
+		i++;
 	}
 	if (compar == 0)
 		return (1);
@@ -36,10 +53,29 @@ int	ft_contains(const char *s1, const char **s2, int i)
 		return (0);
 }
 
+int	ft_isnum(char *num)
+{
+	int	i;
+	int	nnum;
+
+	nnum = 0;
+	i = 0;
+	if (num[i] == '-' || num[i] == '+')
+		i++;
+	while (num[i] && !nnum)
+	{
+		if(num[i] < '0' ||  num[i] > '9')
+			nnum = 1;
+		i++;
+	}
+	return (nnum);
+	
+}
+
 int	ft_validade_num(char **num, int i)
 {
 	int	lerror;
-	int tempnum;
+	int	tempnum;
 
 	lerror = 0;
 	while (num[i] && !lerror)
@@ -52,12 +88,13 @@ int	ft_validade_num(char **num, int i)
 			tempnum = ft_atoi(num[i]);
 			if (tempnum < -2147483648 || tempnum > 2147483647)
 				lerror = 1;
-		}		
+		}
+		i++;		
 	}
 	return (lerror);
 }
 
-int    ft_validate_arg(int argc, char **argv)
+void	ft_validate_arg(int argc, char **argv)
 {
 	int	i;
 	char **list;
@@ -76,17 +113,16 @@ int    ft_validate_arg(int argc, char **argv)
 	{
 		if (argc == 2)
 			ft_free(list);
-		ft_error("Error");
+		write(1, "Error\n", 6);
 	}
-		
 }
    
 int main(int argc,char **argv)
 {
 
 	if(argc == 1)   
-		return (0); 
-	ft_validate_arg(argc, list);
+		return (0);
+	ft_validate_arg(argc, argv);
 	/*
 		if(argc == 2)
 		list = ft_split(argv[1], ' ');
@@ -97,6 +133,6 @@ int main(int argc,char **argv)
 		return (write(1, "error\n", 6));
 	else
 		return (write(1, "ok!", 3));
-	return 0;
 	*/
+	return 0;
 }   
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: faribeir <faribeir@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 09:54:02 by faribeir          #+#    #+#             */
-/*   Updated: 2026/02/14 10:11:48 by faribeir         ###   ########.fr       */
+/*   Updated: 2026/02/18 20:06:53 by faribeir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,10 @@ typedef struct s_node
 	struct s_node	*next;
 }	t_node;
 
-int	ft_validate_arg(int argc, char **list);
+void	ft_validate_arg(int argc, char **argv);
+int	ft_validade_num(char **num, int i);
+int	ft_contains(const char *s1, char **s2, int i);
+int	ft_strcmp(const char *s1, const char *s2);
+int	ft_isnum(char *num);
 
 #endif
