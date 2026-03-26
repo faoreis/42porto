c6beaf3a4212916eeebf8ae6e0991df715962f2d
faoreis <fao.reis123@gmail.com>
2026-03-26 22:11:37 +0000

    module02
    ex0

---
+++ Python/Module02/ex0/ft_first_exception.py
@@ -0,0 +1,22 @@
+def test_temperature(temp_str):
+    try:
+        int(temp_str)
+        print(f'Temperature is now {temp_str}°C')
+    except  ValueError as e:
+        print(f'Caught input_temperature error: {e}')
+
+def input_temperature(temp_str):
+    print(f"Input data is '{temp_str}'")
+    test_temperature(temp_str)
+
+
+
+if __name__ == "__main__":
+    print("=== Garden Temperature ===")
+    print()
+    input_temperature(25)
+    print()
+    input_temperature("abc")
+    print()
+    print("All tests completed - program didn't crash!")
+
