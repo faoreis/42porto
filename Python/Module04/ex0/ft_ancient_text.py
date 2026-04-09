5be579ddb4de4285207a633159ea6e8bdedca8d0
faoreis <fao.reis123@gmail.com>
2026-04-09 14:11:20 +0100

    module04
    ex0

---
+++ Python/Module04/ex0/ft_ancient_text.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def ft_open_file(file: str) -> None:
+    try:
+        print(f"Accessing file '{file}'")
+        file_open = open(file, "r")
+        print("---\n")
+        print(f'{file_open.read()}')
+        file_open.close()
+        print(f"\n---\nFile '{file}' closed.")
+    except FileNotFoundError as error:
+        print(f"Error opening file '{file}': {error}\n")
+    except PermissionError as error:
+        print(f"Error opening file '{file}': {error}\n")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(f'Usage: {sys.argv[0]} <file>')
+    else:
+        print("=== Cyber Archives Recovery ===")
+        ft_open_file(sys.argv[1])
