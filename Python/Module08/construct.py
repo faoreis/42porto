1dd3f6d0b0bb55b1ec3bcff26536f582e14ced36
faoreis <fao.reis123@gmail.com>
2026-05-29 00:32:06 +0100

    commit
    teste

--- Python/Module08/construct.py
+++ Python/Module08/construct.py
@@ -18,6 +18,7 @@ def display(status: bool) -> None:
         print("source matrix_env/bin/activate # On Unix")
         print("matrix_env\Scripts\activate # On Windows\n")
         print("Then run this program again.")
+print("Then run this program again.")
         
 
 if __name__ == "__main__":
